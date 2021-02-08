e69b1d2a3ab1115ef818fa71bbc817b3869627e1
Sam Eriksen-Schultz <sameriksenschultz@gmail.com>
2021-02-07 20:29:53 -0600

    Add files via upload

--- bot.py
+++ bot.py
@@ -5,6 +5,13 @@ from discord.utils import get
 
 client = commands.Bot(command_prefix='.')
 
+@client.event
+async def on_ready():
+    print('Bot is online')
+    activity = discord.Game(name="osu!")
+    await client.change_presence(status=discord.Status.idle, activity=activity)
+
+
 
 @client.command()
 @commands.has_permissions(administrator=True)
@@ -55,4 +62,4 @@ for filename in os.listdir('./cogs'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
 
-client.run('tokenhere')
+client.run('Nzk3NTI1NDM0NzA2NjkwMTE5.X_nvcA.ZzlaNlzXaH21L_SSgGRE_dndHm4')
