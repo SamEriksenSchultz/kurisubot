0d052a9de5d6f094e326d49ca8872b73c70b19c3
Sam Eriksen-Schultz <sameriksenschultz@gmail.com>
2021-02-09 14:23:16 -0600

    Add files via upload

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from discord.utils import get
 
 client = commands.Bot(command_prefix='.')
 
+#ready state and set status
 @client.event
 async def on_ready():
     print('Bot is online')
@@ -12,13 +13,14 @@ async def on_ready():
     await client.change_presence(status=discord.Status.online, activity=activity)
 
 
-
+#load cogs from guild
 @client.command()
 @commands.has_permissions(administrator=True)
 async def load(ctx, extension):
     client.load_extension(f'cogs.{extension}')
     await ctx.send(f'Extension {extension} loaded')
 
+#error handling for load commands
 @load.error
 async def load_error(ctx, error):
     if isinstance(error, commands.CommandInvokeError):
@@ -26,12 +28,18 @@ async def load_error(ctx, error):
     else:
         raise error
 
+#unload cogs from guild
 @client.command()
 @commands.has_permissions(administrator=True)
 async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
     await ctx.send(f'Extension {extension} unloaded')
 
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+#error handling for unload commands
 @unload.error
 async def unload_error(ctx, error):
     if isinstance(error, commands.CommandInvokeError):
@@ -39,12 +47,14 @@ async def unload_error(ctx, error):
     else:
         raise error
 
+#clears "amount" messages from the channel it's used in
 @client.command()
 @commands.has_permissions(manage_messages=True)
 async def clear(ctx, amount=10):
     await ctx.channel.purge(limit=amount)
     await ctx.send(f'Cleared {amount} messages')
 
+#gives members full access when they react to message in #rules
 @client.event
 async def on_raw_reaction_add(payload):
     message_id = payload.message_id
@@ -57,9 +67,7 @@ async def on_raw_reaction_add(payload):
             member = payload.member
             await member.add_roles(role)
 
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[:-3]}')
 
 
-client.run('Nzk3NTI1NDM0NzA2NjkwMTE5.X_nvcA.msuBdo0TYVFmVPoVaaeAd3HJyJc')
+
+client.run('token_here')
